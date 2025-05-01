0e48cf5eb1747dde1200608a09ecc3711b53faf7
Huy Nguyen <danghuy1999@gmail.com>
2025-05-01 17:46:10 +0700

    [c] Add docstring to cover dummy function and module

--- pypackage/{{ cookiecutter.repo_name }}/{{ cookiecutter.project_slug }}/dummy.py
+++ pypackage/{{ cookiecutter.repo_name }}/{{ cookiecutter.project_slug }}/dummy.py
@@ -1,2 +1,21 @@
+"""
+This module provides a simple function for incrementing integer values.
+
+Example:
+    >>> from dummy import dump_func
+    >>> dump_func(5)
+    7
+"""
+
+
 def dump_func(a: int) -> int:
+    """
+    Increments the given integer value by 2.
+
+    Args:
+        a (int): The input integer value.
+
+    Returns:
+        int: The input value incremented by 2.
+    """
     return a + 2

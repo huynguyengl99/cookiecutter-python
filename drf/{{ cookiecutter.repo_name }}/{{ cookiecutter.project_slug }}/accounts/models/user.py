d63d9249f2f56bb569a0aa44fb810d460ebac7f2
Huy Nguyen <danghuy1999@gmail.com>
2025-05-02 00:03:58 +0700

    [c] Fix user typing and str function

--- drf/{{ cookiecutter.repo_name }}/{{ cookiecutter.project_slug }}/accounts/models/user.py
+++ drf/{{ cookiecutter.repo_name }}/{{ cookiecutter.project_slug }}/accounts/models/user.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, ClassVar, Self, TypeVar, cast
+from typing import TYPE_CHECKING, Any, ClassVar, TypeVar, cast
 
 from django.contrib import auth
 from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
@@ -124,7 +124,7 @@ class User(AbstractUser):
         max_length=255,
         unique=True,
     )
-    objects: ClassVar[UserManager[Self]] = UserManager[Self]()  # type: ignore[assignment]
+    objects: ClassVar[UserManager['User']] = UserManager['User']()  # type: ignore[assignment]
 
     username = None  # type: ignore[assignment]
     EMAIL_FIELD = "email"
@@ -132,4 +132,4 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     def __str__(self) -> str:
-        return self.email
+        return str(self.email)
